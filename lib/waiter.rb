6026f56720a59f2f701197c769367609dcd3416e
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-18 16:50:01 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -17,7 +17,7 @@ class Waiter
   def new_meal(customer, total, tip=0)
      Meal.new(self, customer, total, tip)
    end
-   
+
   def meals
     Meal.all.select do |meal|
       meal.waiter == self #checking for waiter now
