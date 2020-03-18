b6d1dc5c0dcd50ddea1ec65293eef89a2029867d
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-18 16:48:45 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -19,7 +19,7 @@ class Waiter
       meal.waiter == self #checking for waiter now
     end
   end
-  
+
   def best_tipper
   best_tipped_meal = meals.max do |meal_a, meal_b|
     meal_a.tip <=> meal_b.tip
